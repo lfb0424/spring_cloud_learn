d18749f775fb9f55c73b6921d06ccce5139955cf
lifubei <lifubei@beyondcent.com>
2019-04-18 11:19:44 +0800

    123

---
+++ hello-service-api/src/main/java/com/example/helloserviceapi/com/learn/dto/User.java
@@ -0,0 +1,43 @@
+package com.example.helloserviceapi.com.learn.dto;
+
+/**
+ * @author : lifubei
+ * @date 2019/04/18
+ */
+public class User {
+
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ hello-service-api/src/main/java/com/example/helloserviceapi/com/learn/service/HelloService.java
@@ -0,0 +1,21 @@
+package com.example.helloserviceapi.com.learn.service;
+
+import com.example.helloserviceapi.com.learn.dto.User;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author lifb
+ * @date 2019/4/18
+ */
+@RequestMapping("/refactor")
+public interface HelloService {
+
+    @RequestMapping(value = "/hello4",method = RequestMethod.GET)
+    String hello(@RequestParam("name") String name);
+
+    @RequestMapping(value = "/hello5",method = RequestMethod.GET)
+    String hello(@RequestHeader("name") String name,@RequestHeader("age")Integer age);
+
+    @RequestMapping(value = "/helo6",method = RequestMethod.GET)
+    String hello(@RequestBody User user);
+}
