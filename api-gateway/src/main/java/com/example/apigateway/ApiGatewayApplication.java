847654c706c0590a5e1745bea65fc76d13659d98
lifubei <lifubei@beyondcent.com>
2019-04-18 16:23:02 +0800

    spring cloud Zuul

---
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.example.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author lifubei
+ * @date 2019/4/18
+ *
+ * @EnableZuulProxy 开启Zuul的API网关服务功能
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
